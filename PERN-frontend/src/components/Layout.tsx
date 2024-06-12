125a576308d39669b1fb29ced2a284b242f4d71e
mario-george <vortexspades@gmail.com>
2024-06-13 01:44:51 +0300

    Add Layout component to PERN-frontend

---
+++ PERN-frontend/src/components/Layout.tsx
@@ -0,0 +1,13 @@
+import { Inter } from "next/font/google";
+import { ChakraProvider } from "@chakra-ui/react";
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ChakraProvider>{children}</ChakraProvider>
+      </body>
+    </html>
+  );
+}
