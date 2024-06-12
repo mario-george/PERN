c821517d9fc90de08ee8a323d926cfdb26c4eb9a
mario-george <vortexspades@gmail.com>
2024-06-13 01:48:57 +0300

    Add API endpoint to create a new todo in PERN-backend

--- PERN-backend/index.ts
+++ PERN-backend/index.ts
@@ -13,6 +13,20 @@ app.use(cors());
 // parse the request body to json
 
 app.use(express.json());
+// add a todo
+app.post(
+  "/todos",
+  catchAsync(async (req: Request, res: Response) => {
+    let { description } = req.body;
+
+    let queryString = `
+  INSERT INTO todo(description) VALUES($1) RETURNING *; 
+`;
+    let newTODo = await pool.query(queryString, [description]);
+console.log("newTODo",newTODo)
+    return res.json({message:"todo has been added successfully",todo:newTODo.rows[0]});
+  })
+);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
