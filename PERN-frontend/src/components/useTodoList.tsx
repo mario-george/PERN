8d5f58f39c0bf7848ad08c97980c022d85556df2
mario-george <vortexspades@gmail.com>
2024-06-13 11:42:11 +0300

    Refactor useTodoList hook to handle error fetching data in PERN-frontend

--- PERN-frontend/src/components/useTodoList.tsx
+++ PERN-frontend/src/components/useTodoList.tsx
@@ -2,18 +2,27 @@ import { useEffect, useState } from "react";
 interface useTodoListParams {
   reloadPage: boolean;
 }
-type ITodoList = [] | { t_id: number; description: string }[];
+export type ITodoList = null|[] | { t_id: number; description: string }[];
 const useTodoList = ({ reloadPage }: useTodoListParams) => {
  const [todoItems,setTodoItems]=useState<ITodoList>([]);
     const fetchItems = async () => {
     const options = {
       headers: { "Content-Type": "application/json" },
     };
+try{
 
-    const res = await fetch(`${process.env.BACKEND_LINK}todos`, options);
-    const resData = await res.json();
+  const res = await fetch(`${process.env.BACKEND_LINK}todos`, options);
+  const resData = await res.json();
+  if(res.status == 200){
     console.log("resData", resData);
-    setTodoItems(resData)  
+    setTodoItems(resData.items)  
+  
+  }else{
+    console.log("Error fetching data")
+  }
+}catch(err){
+console.error(err)
+}
 };
 
   useEffect(() => {
