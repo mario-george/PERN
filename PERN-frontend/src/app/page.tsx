016f1434534f57e812721bb18e67499a747080fd
mario-george <vortexspades@gmail.com>
2024-06-13 11:42:17 +0300

    Add useTodoForm custom hook to manage todo form in PERN-frontend

--- PERN-frontend/src/app/page.tsx
+++ PERN-frontend/src/app/page.tsx
@@ -1,8 +1,12 @@
-import Image from "next/image";
+"use client"
+import useTodoForm from "@/components/useTodoForm";
+
 
 export default function Home() {
+const {TodoFormComponent} =useTodoForm();
   return (
    <>
+   {TodoFormComponent}
    </>
   );
 }
