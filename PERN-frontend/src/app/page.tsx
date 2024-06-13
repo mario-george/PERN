af9d56cf844b1b6e464c624f44c910b325dbfce6
mario-george <vortexspades@gmail.com>
2024-06-13 14:58:55 +0300

    Add "Add a todo task" heading to Home component in PERN-frontend

--- PERN-frontend/src/app/page.tsx
+++ PERN-frontend/src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 import useTodoForm from "@/components/useTodoForm";
-
+import { Text } from '@chakra-ui/react'
 
 export default function Home() {
 const {TodoFormComponent} =useTodoForm();
   return (
    <>
+  <Text fontSize='3xl' className="p-3 m-3">Add a todo task</Text>
+
+
    {TodoFormComponent}
    </>
   );
